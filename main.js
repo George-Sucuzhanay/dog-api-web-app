a4b89069feafa60c4e2aa955f704425d388c284a
George <georgesucuzhanay100@gmail.com>
2021-11-03 22:02:00 -0400

    working on async js functions

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const button = document.querySelector("button")
 const breedInput = document.querySelector("input")
 const imageDiv = document.querySelector("div")
+
+// Add an event listener for the click
+
+button.addEventListener(click =>{
+    
+})
